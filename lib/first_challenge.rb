f49171482c4500e553a8a16500e1152999859f29
Lola Onanuga <slonanuga@gmail.com>
2018-10-02 01:57:21 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if |x|
+contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if do |x|
 x == "strawberry"
 end
 contacts
